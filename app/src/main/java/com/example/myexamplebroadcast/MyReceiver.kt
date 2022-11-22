cd8292e14e8be3d9e06f904519ee8b5bcb1a4e19
Murad <m.iusupov>
2022-11-22 15:45:08 +0300

    add multiple actions in Broadcast receiver

--- app/src/main/java/com/example/myexamplebroadcast/MyReceiver.kt
+++ app/src/main/java/com/example/myexamplebroadcast/MyReceiver.kt
@@ -10,8 +10,16 @@ class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         when (intent?.action) {
             Intent.ACTION_BATTERY_LOW -> {
-            Toast.makeText(context, "Battery low", Toast.LENGTH_SHORT).show()
-        }
+                Toast.makeText(context, "Battery low", Toast.LENGTH_SHORT).show()
+            }
+            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
+                val ternedOn = intent.getBooleanExtra("state", false)
+                Toast.makeText(
+                    context,
+                    "Airplane mode changed. Terned on $ternedOn",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
